6d319038ef08e9c43ce8106c48be53dd42774466
Fabio 4geekAcademy <fabio4geeksacademy@gmail.com>
2022-05-29 23:42:23 -0400

    ListReact initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,116 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './App.css'
+function Iteam(props) {
+  const [edit, setEdit] = useState(false);
+  const [text, setText] = useState(props.iteam);
+  let inputIteamRef = React.useRef();
+
+  useEffect(() => {
+    // componentwillmount in functional component.
+    // Anything in here is fired on component mount.
+    return () => {
+      console.log('******************* UNMOUNTED');
+    };
+  }, []);
+  const editButtom = () => {
+    if (edit) {
+      let value = inputIteamRef.current.value;
+      setText(value)
+      let lista = props.List;
+      lista[props.index] = value;
+      props.handleChange(lista)
+    }
+    setEdit(!edit)
+  }
+
+  const deleteIteam = () => {
+    console.log("pase");
+    let lista = props.List;
+    lista.splice(props.index, 1);
+    console.log("lista ", lista);
+    props.handleChange(lista)
+    console.log("props.List ", props.List);
+  }
+
+  return (
+    <>
+      {!edit &&
+        <li>
+          <span>{text}</span>
+          <button onClick={editButtom}>editar</button>
+          <button onClick={() => deleteIteam()}>eliminar</button>
+        </li>
+      }
+
+      {edit &&
+        <li>
+          <input defaultValue={text} ref={inputIteamRef}></input>
+          <button onClick={editButtom}>editar</button>
+          <button disabled>eliminar</button>
+        </li>
+      }
+    </>
+  )
+}
 
 function App() {
+  const [List, setList] = useState([]);
+  let inputRef = React.useRef();
+
+  useEffect(() => {
+    console.log(" List ", List);
+    setList(List);
+    return () => {
+      console.log('******************* UNMOUNTED');
+    };
+  }, [List])
+
+  const updateList = (list) => {
+    setList(list);
+    inputRef.current.value = ""
+  }
+
+  const addEnter = (event) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      if (event.target.value) {
+        let newList = [...List, event.target.value];
+        updateList(newList)
+      }
+    }
+  }
+
+  const addButom = (text) => {
+    if (text) {
+      let newList = [...List, text];
+      updateList(newList)
+    }
+  }
+
+  const handleChange = (newValue) => {
+    console.log("newValue handleChange ", newValue)
+    console.log("List handleChange ", List)
+    setList(newValue);
+    console.log("new List handleChange ", List)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Lista</h1>
+      <h2>{List.length}</h2>
+      <label>
+        <input type="text" name="lista" ref={inputRef} onKeyPress={(event) => addEnter(event)} />
+        <button onClick={() => addButom(inputRef.current.value)}>ingresa un elemento</button>
+      </label>
+
+      <ul id="lista">
+        {List &&
+          List.map((element, index) => {
+            return <Iteam iteam={element} key={index} index={index} List={List} handleChange={handleChange}></Iteam>
+          })
+        }
+      </ul>
+    </>
   );
 }
 
